d055514d881cb4638dc2b479d0db4cedcefdfedc
beccoder <bekhzodkhudoyarov@gmail.com>
2023-03-26 16:57:41 +0300

    2

--- game/main.go
+++ game/main.go
@@ -1,9 +1,83 @@
 package main
 
+import (
+	"strings"
+	"fmt"
+)
 /*
 	код писать в этом файле
 	наверняка у вас будут какие-то структуры с методами, глобальные перменные ( тут можно ), функции
 */
+
+
+type Room struct {
+	Name string
+	InitMessage string
+	NextRoom []string
+	InRoom map[string][]string
+}
+
+type Person struct {
+	// CurrentRoom string
+	Bag bool
+	Have []string
+	Mission []string
+	// Rooms map[string]Room
+}
+
+func (person *Person) Glance() string {
+		var result string
+		result += room.InitMessage
+		if len(room.InRoom) > 0 {
+			for place, items := range room.InRoom {
+				result = result + ", " + place + ": "
+				for index, item := range items {
+					if index == 0 {
+						result += item
+					} else {
+						result += ", " + item
+					}
+				}
+			}
+		}
+		if room.Name == "kitchen" {
+			result += ", " + person.Mission
+			// result += ", надо "
+			// for index, mission := range person.Mission {
+			// 	if index == 0 {
+			// 		result += mission
+			// 	} else {
+			// 		result += " и " + mission
+			// 	}
+			// } 
+		}
+		if len(room.NextRoom) > 0 {
+			result += ". можно пройти - "
+			for index, path := range room.NextRoom {
+				if index == 0 {
+					result += path
+				} else {
+					result += ", " + path
+				}
+			}
+		}
+}
+
+func (room *Room) Move() {
+	// 
+}
+
+func (room *Room) Wear() {
+	
+}
+
+func (room *Room) Take() {
+	// 
+}
+
+func (room *Room) Apply {
+	// 
+}
 
 func main() {
 	/*
@@ -25,5 +99,7 @@ func handleCommand(command string) string {
 		данная функция принимает команду от "пользователя"
 		и наверняка вызывает какой-то другой метод или функцию у "мира" - списка комнат
 	*/
+	var result string
+	
 	return "not implemented"
 }
